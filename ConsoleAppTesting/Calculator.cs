797ff965b6677ff4cd83f24ca61747fc53f2e43f
daniellkarminsky <daniellkarminsky@gmail.com>
2024-05-15 20:09:34 +0300

    Add project files.

---
+++ ConsoleAppTesting/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTesting
+{
+    public class Calculator
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+    public class CalculatorTests
+    {
+        public void MultiplyTest()
+        {
+            var calculator = new Calculator();
+            if (calculator.Multiply(4, 4) == 16)
+            {
+                Console.WriteLine("Multiply passed.");
+            }
+            else
+            {
+                Console.WriteLine("Multiply failed.");
+            }
+        }
+    }
+}
---
+++ ConsoleAppTesting/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleAppTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var calculatorTests = new CalculatorTests();
+
+            Console.WriteLine("Running tests");
+
+            try
+            {
+                calculatorTests.MultiplyTest();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error running test: {ex.Message}");
+            }
+        }
+    }
+}
